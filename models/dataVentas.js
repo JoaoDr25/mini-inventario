0e6bc61db9cb33ae93101ece75a566a46bdd963c
Juan Camilo Davila R <jcamilodavila25@gmail.com>
2025-06-19 00:26:11 -0500

    Cambios

--- models/dataVentas.js
+++ models/dataVentas.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
 const ventasSchema = new mongoose.Schema ({
-    
+    cliente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cliente',
+        required: true
+    },
+    productos : [
+        {
+            producto: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Producto',
+                required: true
+            }
+            
+        }
+    ]
 })
